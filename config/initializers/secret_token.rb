d1130cb05f981a8b630ae675b8736ca2b0933735
Qihuan Piao <fantasyday@gmail.com>
2012-10-20 00:25:29 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quoty::Application.config.secret_token = 'bfb93826ac124ac23cc047d99dc31fbaaa22ad3fe34ebc529e355018473cc3e477e14c2ad220f8a2bca72c5c10c3df641f7aef5a9696a5b2a846405da0dccf75'
