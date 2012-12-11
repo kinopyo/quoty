b950713585f70249fc83854d5212a4ce57d0d499
Qihuan Piao <qihuan-piao@cookpad.com>
2012-12-11 11:49:31 +0900

    [wip] about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Quoty::Application.routes.draw do
+  # sessions
   match '/auth/:provider/callback', to: 'sessions#create'
   match '/auth/failure', to: 'sessions#failure'
   match '/login', to: 'sessions#new'
   match '/logout', to: 'sessions#destroy'
+
+  get 'about', to: 'pages#about'
 
   match '/language/:language', to: 'quotes#language', language: /[a-z]+/, as: :language
   resources :quotes do
