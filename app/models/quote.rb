e924760649af411dbb0e270b57a3cbe71a2f5be5
Qihuan Piao <qihuan-piao@cookpad.com>
2012-12-12 11:25:15 +0900

    Fix language detection

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -15,7 +15,7 @@ class Quote < ActiveRecord::Base
 
   def detect_and_set_language
      result = CLD.detect_language(content)
-     self.language = result[:name].downcase if result[:reliable]
+     self.language = result[:name].downcase# if result[:reliable]
   end
 
   def language_in_its_own
