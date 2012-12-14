01c74e0c75280b9213c83e64bcb25194594d7e0b
Qihuan Piao <fantasyday@gmail.com>
2012-12-14 23:13:29 +0900

    Bug fix

--- app/observers/vote_observer.rb
+++ app/observers/vote_observer.rb
@@ -1,5 +1,5 @@
 class VoteObserver < ActiveRecord::Observer
   def after_destroy(vote)
-    PublicActivity::Activity.where(trackable_type: 'Vote', trackable_id: self.id).destroy_all
+    PublicActivity::Activity.where(trackable_type: 'Vote', trackable_id: vote.id).destroy_all
   end
 end
