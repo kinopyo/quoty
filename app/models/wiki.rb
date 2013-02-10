8bcd7afc165a70cf86a0568f39ff2d88287d3198
kinopyo <fantasyday@gmail.com>
2013-02-10 19:30:54 +0900

    wiki has many quotes

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -3,6 +3,14 @@ class Wiki < ActiveRecord::Base
 
   attr_accessible :description, :title, :parent_id
   belongs_to :user
+  has_many :quotes, class_name: 'Quote', finder_sql: Proc.new {
+    %Q{
+      SELECT *
+      FROM quotes q
+      WHERE q.author_wiki_id = #{id}
+      OR q.source_wiki_id = #{id}
+    }
+  }
 
   validates :title, presence: true
 
