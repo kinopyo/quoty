720eb01387c4b1fd4b731db93678d746212a093a
kinopyo <fantasyday@gmail.com>
2013-02-10 19:35:06 +0900

    delete unused wiki resources

--- spec/factories/wikis.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :wiki do
-    title "MyString"
-    description "MyString"
-    ancestry "MyString"
-  end
-end
