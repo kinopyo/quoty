87b65098764ee8b1cc1144170c1946c60b5aece9
kinopyo <fantasyday@gmail.com>
2013-06-22 01:32:10 +0900

    Enable :focus metadata to run focused specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,6 +52,10 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
   config.include FactoryGirl::Syntax::Methods
 
   config.include(MailerMacros)
