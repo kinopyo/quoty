aef9a440127983e108969947de042566aa0a644d
kinopyo <fantasyday@gmail.com>
2013-06-27 23:41:24 +0900

    Fix regular expression of email
    > The provided regular expression is using multiline anchors (^ or $), which may present a security risk. Did you mean to use \A and \z, or forgot to add the :multiline => true option?

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -3,6 +3,6 @@ class Identity <OmniAuth::Identity::Models::ActiveRecord
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, allow_blank: true, format:
-    { with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
+    { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :password, confirmation: true, length: { in: 6..20 }
 end
