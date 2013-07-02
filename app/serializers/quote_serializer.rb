e3d06e95a97c3cb70ec9cb398c030fdb0267842f
kinopyo <fantasyday@gmail.com>
2013-07-02 23:35:14 +0900

    Cache quote serializer

--- app/serializers/quote_serializer.rb
+++ app/serializers/quote_serializer.rb
@@ -1,9 +1,15 @@
 class QuoteSerializer < ActiveModel::Serializer
+  cached
+
   attributes :id, :author, :content, :source, :language, :context, :created_at
   has_one :user, key: :creator, serializer: UserShortSerializer
   has_many :photos, key: :images, serializer: PhotoSerializer
 
   def created_at
     object.created_at.to_time.iso8601
+  end
+
+  def cache_key
+    object
   end
 end
