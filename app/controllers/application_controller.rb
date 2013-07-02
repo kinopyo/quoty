96fd2b0ae978395c29769c593a481b429b7cd568
kinopyo <fantasyday@gmail.com>
2013-07-02 19:42:08 +0900

    Return 'desktop' when can't get user agent from header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -88,10 +88,11 @@ class ApplicationController < ActionController::Base
   MOBILE_BROWSERS = %w(playbook windows phone android ipod iphone opera mini blackberry palm hiptop avantgo plucker xiino blazer elaine windows ce; ppc; windows ce; smartphone; windows ce; iemobile up.browser up.link mmp symbian smartphone midp wap vodafone o2 pocket kindle mobile pda psp treo)
 
   def detect_browser
-    agent = request.headers['HTTP_USER_AGENT'].downcase
+    agent = request.headers['HTTP_USER_AGENT']
+    return 'desktop' unless agent
 
     MOBILE_BROWSERS.each do |m|
-      return 'mobile' if agent.match(m)
+      return 'mobile' if agent.downcase.match(m)
     end
 
     'desktop'
