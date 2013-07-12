52f7c5c90c277fe4c0c66bbddfb18a5309d6b14e
kinopyo <fantasyday@gmail.com>
2013-07-12 20:00:10 +0900

    Limit activities to recent 10 records

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,7 @@
 class ActivitiesController < ApplicationController
   def index
     @user = User.friendly.find(params[:user_id])
-    @activities = @user.activities.order('created_at DESC')
+    @activities = @user.activities.order('created_at DESC').limit(10)
 
     respond_to do |format|
       format.html
