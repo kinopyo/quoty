ef041f47b1755a7d44f32411b1aef97d1878bf3d
kinopyo <fantasyday@gmail.com>
2013-07-15 23:31:50 +0900

    Add more expectation to quote feature spec

--- spec/features/quote_spec.rb
+++ spec/features/quote_spec.rb
@@ -6,7 +6,10 @@ describe 'Quote' do
     visit new_quote_path
     fill_in 'quote_content', with: 'my quote'
     fill_in 'quote_author_name', with: 'the author'
+    fill_in 'quote_source', with: 'book'
     click_button 'Create'
     page.should have_content('my quote')
+    page.should have_content('the author')
+    page.should have_content('book')
   end
 end
