add1bfcfa839276bbdda9c9d030f7ba9fd911cf2
kinopyo <fantasyday@gmail.com>
2013-07-20 14:34:32 +0900

    Add search logic from controller to model

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,9 +4,7 @@ class AuthorsController < ApplicationController
   end
 
   def search
-    # TODO should be able to find author by name or alias, like nickname. Also
-    # should be able to find either lowercase or uppercase.
-    @authors = Author.where("name LIKE ?", "%#{params[:query]}%")
+    @authors = Author.search(params[:query])
     render json: @authors.as_json(only: [:id, :name])
   end
 end
