31ce45ba8da76aa96afe5bb74be40d14ffb7dd64
kinopyo <fantasyday@gmail.com>
2013-07-20 23:47:03 +0900

    Merge traditional chinese to just chinese

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -53,7 +53,9 @@ class Quote < ActiveRecord::Base
 
   def detect_and_set_language
      result = CLD.detect_language(content)
-     self.language = result[:name].downcase# if result[:reliable]
+     lang = result[:name].downcase
+     lang = 'chinese' if lang == 'chineset'
+     self.language = lang
   end
 
   def find_or_create_author_and_source_wiki
