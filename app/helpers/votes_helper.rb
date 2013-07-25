7c08efc75fb827d9c36fd07fae71dd59a1ff3302
kinopyo <fantasyday@gmail.com>
2013-07-26 01:15:11 +0900

    MVP for "like" instead of "vote"

--- app/helpers/votes_helper.rb
+++
@@ -1,9 +0,0 @@
-module VotesHelper
-  def vote_class(score, vote)
-    # use in-memory find to avoid N+1 queries
-    return unless vote
-
-    return 'active' if score == :up && vote.score == 1
-    return 'active' if score == :down && vote.score == -1
-  end
-end
